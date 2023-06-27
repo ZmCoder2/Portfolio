dd42606d7b790944402dd1276e2f87978ecc2fce
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-26 21:46:18 -0400

    resume image

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,29 +1,31 @@
-import { useState } from 'react'
-// useEffect,
-import Loader from 'react-loaders'
-import AnimatedCharacters from '../AnimatedCharacters'
-import "./index.scss"
+import { useState } from 'react';
+import Loader from 'react-loaders';
+import AnimatedCharacters from '../AnimatedCharacters';
+import resumeImage from '../../assets/resume.png';
+import './index.scss';
 
 const Resume = () => {
-    const [characterClass] = useState('text-animate')
-    // , setCharacterClass
-    return (
-        <>
-            <div className="conatainer resume-page">
-                <div className="text-zone">
-                    <h1>
-                        <AnimatedCharacters
-                            characterClass={characterClass}
-                            characterArray={['R', 'e', 's', 'u', 'm', 'e']}
-                            index={15}
-                        />
-                    </h1>
-                    {}
-                    </div>
-                    </div>
-                    <Loader type="cube-transition" />
-                    </>
-)
-}
+  const [characterClass] = useState('text-animate');
 
-export default Resume
+  return (
+    <>
+      <div className="container resume-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedCharacters
+              characterClass={characterClass}
+              characterArray={['R', 'e', 's', 'u', 'm', 'e']}
+              index={15}
+            />
+          </h1>
+          <div className="image-zone">
+            <img src={resumeImage} alt="Resume" />
+          </div>
+        </div>
+      </div>
+      <Loader type="cube-transition" />
+    </>
+  );
+};
+
+export default Resume;
